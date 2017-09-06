24c6c5c3d7971e1c90caa2d7612aefa5cd47ad2d
abbas886 <abbas886@yahoo.com>
2017-09-06 14:29:55 +0530

    This class not required

--- src/main/java/com/stackroute/activitystream/dao/BackEndRestController.java
+++ src/main/java/com/stackroute/activitystream/dao/BackEndRestController.java
@@ -3,6 +3,7 @@ package com.stackroute.activitystream.dao;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//This class not required
 @RestController
 public class BackEndRestController {
 
