42a0c857e30ba6cf19c515e9ab689a2f59452bc1
abbas886 <abbas886@yahoo.com>
2017-09-06 14:39:35 +0530

    update method signature.

--- src/main/java/com/stackroute/activitystream/dao/UserDAO.java
+++ src/main/java/com/stackroute/activitystream/dao/UserDAO.java
@@ -8,6 +8,8 @@ public interface UserDAO {
 
 	public boolean saveUser(User user);
 	public User getUserByEmailId(String emailId);
+	//we should able to delete if we have emailId
+	//So the parameter should be only emailId, not complete user object.
 	public boolean deleteUser(User user);
 	public boolean updateUser(User user);
 	public List<User> getAllUsers();
