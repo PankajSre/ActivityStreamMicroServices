bff0470cc51802c0acd58f76afd9a416834aa2e7
abbas886 <abbas886@yahoo.com>
2017-09-06 14:33:01 +0530

    delete unnecessary methods

--- src/main/java/com/stackroute/activitystream/dao/CircleDAO.java
+++ src/main/java/com/stackroute/activitystream/dao/CircleDAO.java
@@ -7,10 +7,13 @@ import com.stackroute.activitystream.model.Circle;
 public interface CircleDAO {
 
 	boolean addCircle(Circle circle);
+	//This method should be in UserCircleDAO.  In circle table/model emailId should not be there.
 	boolean addUserToCircle(String emailId,String circleName);
 	List<Circle> getAllCircles();
 	boolean deleteCircle(String circleName,String ownerId);
 	boolean updateCircle(Circle circle);
 	public Circle getCircleByName(String circleName);
+	
+	//This method is not required as getCircleByName is already there.
 	public boolean isCircleExists(String circleName);//Remove It
 }
